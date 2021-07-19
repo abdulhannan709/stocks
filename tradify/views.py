015b0786b3bb3ac78cc4d59828be1909d0dc3d8f
abdulhanan-fecundity <abdul.hanan@fecundity.io>
2021-07-19 16:31:52 +0500

    Add login signup functionality.

--- tradify/views.py
+++ tradify/views.py
@@ -7,37 +7,69 @@ def index(request):
 
 def charts(request):
         
-    # import pandas as pd
-    # from pandas_datareader import data as web
-    # import plotly.graph_objects as go
+    import pandas as pd
+    from pandas_datareader import data as web
+    import plotly.graph_objects as go
 
-    # stock = 'MSFT'
+    stock = 'MSFT'
  
-    # df = web.DataReader(stock, data_source='yahoo', start='01-01-2019')
+    df = web.DataReader(stock, data_source='yahoo', start='01-01-2019')
+    print(df)
 
-    # trace1 = {
-    #     'x': df.index,
-    #     'open': df.Open,
-    #     'close': df.Close,
-    #     'high': df.High,
-    #     'low': df.Low,
-    #     'type': 'candlestick',
-    #     'name': 'MSFT',
-    #     'showlegend': True
-    # }
+    trace1 = {
+        'x': df.index,
+        'open': df.Open,
+        'close': df.Close,
+        'high': df.High,
+        'low': df.Low,
+        'type': 'candlestick',
+        'name': 'MSFT',
+        'showlegend': True
+    }
+    # Calculate and define moving average of 30 periods
+    avg_30 = df.Close.rolling(window=30, min_periods=1).mean()
 
-    # data = [trace1]
-    # # Config graph layout
-    # layout = go.Layout({
-    #     'title': {
-    #         'text': 'Microsoft(MSFT) Moving Averages',
-    #         'font': {
-    #             'size': 15
-    #         }
-    #     }
-    # })
-    # return render(request, 'charts.html', {'response': data})
-    return render(request, 'charts.html')
+    # Calculate and define moving average of 50 periods
+    avg_50 = df.Close.rolling(window=50, min_periods=1).mean()
+    trace2 = {
+        'x': df.index,
+        'y': avg_30,
+        'type': 'scatter',
+        'mode': 'lines',
+        'line': {
+            'width': 1,
+            'color': 'blue'
+                },
+        'name': 'Moving Average of 30 periods'
+    }
+
+    trace3 = {
+        'x': df.index,
+        'y': avg_50,
+        'type': 'scatter',
+        'mode': 'lines',
+        'line': {
+            'width': 1,
+            'color': 'red'
+        },
+        'name': 'Moving Average of 50 periods'
+    }
+    data = [trace1, trace2]
+    # Config graph layout
+    layout = go.Layout({
+        'title': {
+            'text': 'BBAS3 - BRASIL ON',
+            'font': {
+                'size': 15
+            }
+        }
+    })
+    fig = go.Figure(data=data, layout=layout)
+    fig.write_html("./BBAS3-web.html")
+    fig.show()
+
+    return render(request, 'charts.html', {'response': data})
+    # return render(request, 'charts.html')
 
 def uvolume(request):
     import requests
